2581800cf602bab0601c34e7ac170d8534955db1
Rimshad Ahamed <rimshadahamed@Rimshads-MacBook-Air.local>
2024-02-22 04:46:31 +0530

    Design: Animation

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const sr = ScrollReveal ({
+    distance: '35px',
+    duration: 2400,
+    reset: true
+});
+
+sr.reveal('.logo1',{delay:210, origin: 'left'});
+sr.reveal('.navbar',{delay:310, origin: 'top'});
+sr.reveal('.btn',{delay:410, origin: 'right'});
+
+sr.reveal('.hero h4',{delay:510, origin: 'top'});
+sr.reveal('.hero h2',{delay:610, origin: 'left'});
+sr.reveal('.hero p',{delay:710, origin: 'right'});
+sr.reveal('.social-icons',{delay:810, origin: 'left'});
+sr.reveal('.hero-btn',{delay:910, origin: 'top'});
+sr.reveal('.social-icons',{delay:810, origin: 'left'});
